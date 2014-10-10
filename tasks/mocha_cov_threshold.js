3d68dbab610d5883500806297016b55431f30e49
davetayls <dave@the-taylors.org>
2014-10-10 10:08:53 +0100

    fix old coverage variable

--- tasks/mocha_cov_threshold.js
+++ tasks/mocha_cov_threshold.js
@@ -1,6 +1,7 @@
 
 module.exports = function(grunt){
   grunt.registerMultiTask('mocha_cov_threshold', function(){
+    
     var options = this.options({
       coverage: 60,
       jsonPath: 'test/coverage.json'
@@ -8,9 +9,9 @@ module.exports = function(grunt){
 
     var json = grunt.file.readJSON(options.jsonPath);
     if (json && json.coverage){
-      grunt.log.writeln('Code coverage at', parseInt(coverage.coverage) + '%');
+      grunt.log.writeln('Code coverage at', parseInt(json.coverage) + '%');
       if (json.coverage < options.coverage){
-        grunt.log.error('Code coverage less than limit ', COVERAGE_LIMIT);
+        grunt.log.error('Code coverage less than limit ', options.coverage);
       }
     } else {
       grunt.log.writeln('No coverage found');

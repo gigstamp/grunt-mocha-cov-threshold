f029b1fdf763a7c6f98cfdbfddc2aab08ce46c36
davetayls <dave@the-taylors.org>
2014-12-13 10:26:16 +0000

    output spec logging

--- threshold-cov.js
+++ threshold-cov.js
@@ -3,28 +3,20 @@
  * Module dependencies.
  */
 
-var JSONCov = require('grunt-mocha-cov/node_modules/mocha/lib/reporters/json-cov')
-  , fs = require('fs');
+var JSONCov = require('grunt-mocha-cov/node_modules/mocha/lib/reporters/json-cov');
+var SpecReporter = require('grunt-mocha-cov/node_modules/mocha/lib/reporters/spec');
+var fs = require('fs');
 
-/**
- * Expose `HTMLCov`.
- */
+exports = module.exports = CoverageThresholdSpec;
 
-exports = module.exports = HTMLCov;
-
-/**
- * Initialize a new `JsCoverage` reporter.
- *
- * @param {Runner} runner
- * @api public
- */
-function HTMLCov(runner) {
+function CoverageThresholdSpec(runner) {
   var self = this;
-
+  var spec = new SpecReporter(runner);
   JSONCov.call(this, runner, false);
-
   runner.on('end', function(){
     fs.writeFileSync('coverage.json', JSON.stringify(self.cov));
   });
 }
+CoverageThresholdSpec.prototype = Object.create(JSONCov.prototype);
+CoverageThresholdSpec.prototype.constructor = CoverageThresholdSpec;
 
